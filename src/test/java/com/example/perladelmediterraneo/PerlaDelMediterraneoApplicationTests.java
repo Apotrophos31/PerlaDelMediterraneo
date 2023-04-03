ad1667387edd7444d2f7c7ad977520dc59a558e4
Apotrophos31 <caristi.filippo@gmail.com>
2023-04-03 18:07:17 +0200

    Initial commit

---
+++ src/test/java/com/example/perladelmediterraneo/PerlaDelMediterraneoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.perladelmediterraneo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerlaDelMediterraneoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
